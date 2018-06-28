dfee05a018e757f09e0140105ae58f24696ac95a
亢雄伟 <kangxiongwei@meituan.com>
2018-06-28 21:13:21 +0800

    多子模块配置

---
+++ bms-web/src/main/java/org/kxw/bms/web/Application.java
@@ -0,0 +1,12 @@
+package org.kxw.bms.web;
+/**
+ * Create by kangxiongwei on 2018/6/28 10:09.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("启动工程");
+    }
+
+
+}
