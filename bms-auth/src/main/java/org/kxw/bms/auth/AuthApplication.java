4b4162f66246a463a67e402e771d3d6d69ab50c7
kangxiongwei <kangxiongwei@163.com>
2018-08-12 12:42:35 +0800

    解决本地依赖springboot子模块依赖不到的情况

---
+++ bms-auth/src/main/java/org/kxw/bms/auth/AuthApplication.java
@@ -0,0 +1,16 @@
+package org.kxw.bms.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create by kangxiongwei on 2018/8/11 下午5:30
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+gradle clean build -x test --stacktrace -i
